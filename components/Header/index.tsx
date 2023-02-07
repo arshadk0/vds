0fbe788702633a766ece5a978a91b9954d60693f
Arshad Khan <108132098+arshadk0@users.noreply.github.com>
2023-02-07 15:28:25 +0530

    Initialize home-page

---
+++ components/Header/index.tsx
@@ -0,0 +1,39 @@
+/** @jsxImportSource @emotion/react */
+import { ENSAvatar } from "@web3uikit/web3";
+import * as styles from "./styles";
+import { ConnectWallet } from "@thirdweb-dev/react";
+import Button from "../shared/Button";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+
+const Header = () => {
+  const router = useRouter();
+ 
+  return (
+    <div css={styles.header}>
+      <div css={styles.topBar}>
+        <div css={styles.webName}>WEB3-DOCS</div>
+        <div css={styles.subHeading} onClick={()=>{router.push("/docs")}}>
+          Docs
+        </div>
+        <div css={styles.subHeading}>
+          FAQs
+        </div>
+        <div css={styles.subHeading}>
+          About Us
+        </div>
+      </div>
+      
+      <div css={styles.loginStatus}>
+        <Button type="link" onClick={() => { }} style={styles.buttonStyle}>Get Started</Button>
+        <div css={{position:"relative"}}>
+          <Button type="link" onClick={() => { }} style={styles.buttonStyle}>Login</Button>
+            <div css={styles.loginButton}>
+              <ConnectWallet btnTitle={"Login"} css={styles.loginButton} />
+            </div>
+          </div>
+      </div>
+    </div>
+  );
+};
+export default Header;
