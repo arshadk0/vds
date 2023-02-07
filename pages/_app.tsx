cfc327a6fff7ac2f144feb1bf53dbb81bf6be03a
Arshad Khan <108132098+arshadk0@users.noreply.github.com>
2023-02-07 15:28:26 +0530

    Initialize verifyFlow

---
+++ pages/_app.tsx
@@ -0,0 +1,19 @@
+import type { AppProps } from "next/app";
+import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
+import "../styles/globals.css";
+import ContractHandler from "./../components/UserHome/Contract"
+
+// This is the chainId your dApp will work on.
+const activeChainId = ChainId.Mainnet;
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ContractHandler>
+      <ThirdwebProvider desiredChainId={activeChainId}>
+        <Component {...pageProps} />
+      </ThirdwebProvider>
+    </ContractHandler>
+  );
+}
+
+export default MyApp;
